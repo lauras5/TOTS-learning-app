9a5929c1ea3b14c502322fb35082865717a3055b
Lance Lee <lancechoelee@gmail.com>
2018-06-16 06:49:02 -0700

    attempt 1 at numbergame model and early numbergame controller

--- models/numbergame.server.model.js
+++ models/numbergame.server.model.js
@@ -4,21 +4,43 @@ var Schema = mongoose.Schema
 var NumberGameSchema = new Schema({
     question: String,
     questionImage: String,
-    choices: [String],
+    choices: [Number],
     correctAnswerIndex: Number
 });
 
 NumberGameSchema.methods.populateNumberGameQuestionsData = function populateNumberGameQuestionsData (cb) {
     const numberGameQuestionArray = [
         {
-            question: "How Many",
-            questionImage: ""
-            choices:, 
-            correctAnswerIndex: 
+            question: "How Many Apples Are There?",
+            questionImage: "",
+            choices:[1, 3, 4, 6], 
+            correctAnswerIndex: 2
+        },
+        {
+            question: "How Many Oranges Are There?",
+            questionImage: "",
+            choices:[6, 4, 9, 5], 
+            correctAnswerIndex: 0
+        },
+        {
+            question: "How Many Strawberries Are There?",
+            questionImage: "",
+            choices:[1, 2, 3, 4], 
+            correctAnswerIndex: 2
+        },
+        {
+            question: "How Many Cherries Are There?",
+            questionImage: "",
+            choices:[1, 2, 5, 10], 
+            correctAnswerIndex: 1
+        },
+        {
+            question: "How Many Cookies Are There?",
+            questionImage: "",
+            choices:[2, 3, 4, 5], 
+            correctAnswerIndex: 3
         }
     ]
-
-    
     this.model('NumberGame').save(error => {
         if (error) throw error
     })
--- server.js
+++ server.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/projectDB';
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/timeoutDB';
 
 mongoose.Promise = Promise;
 
