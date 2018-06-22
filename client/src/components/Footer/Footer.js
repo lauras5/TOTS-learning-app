4d095e1edd27b932b7659a6806880b5f5d714bd0
Alvin Ayusa <alvinayusa@dhcp-v086-095.mobile.uci.edu>
2018-06-21 21:20:31 -0700

    loginp page design

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -5,13 +5,13 @@ class Footer extends Component {
     render() {
         return (
             <footer className="page-footer cyan">
-                <div className="container">
+                {/* <div className="container">
                     <div className="footer-copyright">
                         <div className="container">
                             © 2018 Team Final Project
                     </div>
                     </div>
-                </div>
+                </div> */}
             </footer>
         )
     }
--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -7,22 +7,38 @@ class Login extends Component {
         login: 'false'
     };
 
-
     // login functions go here
     render() {
         return (
             <Fragment>
-                <div>I am the Login page</div>
-                <div className='card tree'>
-                    <form action="" name='loginForm'>
-                        <label htmlFor="username">Username</label>
-                        <input id='userInput' type="text" name='username' />
-                        <label htmlFor="email">Email</label>
-                        <input id='userInput' type="text" name='username' />
-                        <label htmlFor="username">Password</label>
-                        <input id='userInput' type="text" name='username' />
-                        <button id='submitBtn'>Let's Go!</button>
-                    </form>
+                <div className='container'>
+                    <h1>Login Page</h1>
+                    <div className='row'>
+                        <div className='col s3'>
+                            <img id='monkey' src='./images/monkey.png' alt="lemur" width="250" height="350" />
+                        </div>
+
+                        <div className='col s6'>
+                            <div id='loginBox' className="card blue darken-4">
+                                <div className="card-content white-text">
+                                    <span class="card-title">Existing Members Login Here</span>
+                                    <form action="" name='loginForm'>
+                                        <input placeholder='Username' id='userInput' type="text" name='username' />
+                                        <input placeholder='Password' id='userInput' type="text" name='username' />
+                                        <button id='submitBtn'>Let's Go!</button>
+                                        <br></br>
+                                        <span>Not a Member Yet? Please click here.</span>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className='col s3'>
+
+                        </div>
+
+
+                    </div>
                 </div>
             </Fragment>
 
