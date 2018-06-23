61bd6e0943bd2d04dc8278d35df7008e15a91c12
Lance Lee <lancechoelee@gmail.com>
2018-06-23 13:43:40 -0700

    added one missing routes reference on serverjs

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path')
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const routes = require('./routes')
 
 
 const PORT = process.env.PORT || 3001;
