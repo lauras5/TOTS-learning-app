8c556c6f4ca6f81d958be580333de8b432d67869
Lance Lee <lancechoelee@gmail.com>
2018-06-26 14:32:02 -0700

    clean up part 1

--- client/src/components/NumberGame/numberGame.js
+++ client/src/components/NumberGame/numberGame.js
@@ -58,27 +58,26 @@ class NumberGame extends Component {
         console.log('Array has been successfully shuffled')
     };
 
+    // Updates scores/stats on page
+    updateScore = answerChoice => {
+        if(parseInt(answerChoice) === parseInt(this.state.topQuestionChoices[this.state.topQuestion.correctAnswerIndex])) {
+            this.state.correctCount++
+        } else {
+            this.state.incorrectCount++
+        };
+    };
+
+
     // handles answer selection from user
     handleClick = answerChoice => {
-        
-        if(parseInt(answerChoice) === parseInt(this.state.topQuestionChoices[this.state.topQuestion.correctAnswerIndex])) {
-            this.state.correctAnswer++
-        } else {
-            this.state.incorrectAnswer++
-        };
-        // console.log('clicked: ' + answerChoice)
-        // console.log('List: ' + JSON.stringify(this.state.numberQuestionList))
-        // console.log('TopQuestion: ' + JSON.stringify(this.state.topQuestion))
-        // console.log('Choice1: ' + this.state.topQuestionChoices[0])
-        // console.log('Choice2: ' + this.state.topQuestionChoices[1])
-        // console.log('Choice3: ' + this.state.topQuestionChoices[2])
-        // console.log('Choice4: ' + this.state.topQuestionChoices[3])
+        this.updateScore(answerChoice)
 
         // Pop first question from numberQuestionList
         this.state.numberQuestionList.splice(0,1)
-        console.log('popped: ' + JSON.stringify(this.state.numberQuestionList))
+        // console.log('popped: ' + JSON.stringify(this.state.numberQuestionList))
         
         if (this.state.numberQuestionList.length === 0){
+            // create this method with POST?
             this.saveGameStats()
             // Replay?
         };
@@ -96,24 +95,28 @@ class NumberGame extends Component {
                 <h1> Number Game </h1>
                 <div className="container">
 
-                    <div id="modal1" class="modal">
-                        <div class="modal-content">
-                        <h4>Modal Header</h4>
-                        <p>A bunch of text</p>
-                        </div>
-                        <div class="modal-footer">
-                        <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
-                        </div>
-                    </div>
+                    {/* <div>
+                        <Modal open = {this.state.open} onClose={this.onCloseModal} center>
+                        <h3>Color Game Score</h3>
+                        <h4 className= "modalStatsCorrect">{colorGameCorrect}</h4>  
+                        <h4 className = "modalStatsIncorrect">{colorGameWrong}</h4> 
+                        
+                        <h3 id="playAgainModalText">Play again?</h3>
+                            <card col-6 id="colorPlayAgain" onClick={this.handleClickPlayAgain}><img src="https://i.pinimg.com/originals/f0/8b/99/f08b998f7548448a73134f4d21c4b5f3.gif" /></card>
+                            <card col-6 id="colorNotPlayAgain" onClick={this.handleClickNotPlayAgain}><img src="https://www.smileysapp.com/gif-emoji/thumbs-down.gif" /></card>
+                        </Modal>
+                    </div> */}
 
                     <img src='http://placehold.it/150' className="numberGameImage"/>
                     <br></br>
-                    <form action="POST">
-                        <button className="waves-effect waves-light btn numberGameBtn" buttonId="btn-1" data-answerChoice={this.state.topQuestionChoices[0]} onClick={() => this.handleClick(this.state.topQuestionChoices[0])}>{this.state.topQuestionChoices[0]}</button>
-                        <button className="waves-effect waves-light btn numberGameBtn" buttonId="btn-2" data-answerChoice={this.state.topQuestionChoices[1]} onClick={() => this.handleClick(this.state.topQuestionChoices[1])}>{this.state.topQuestionChoices[1]}</button>
-                        <button className="waves-effect waves-light btn numberGameBtn" buttonId="btn-3" data-answerChoice={this.state.topQuestionChoices[2]} onClick={() => this.handleClick(this.state.topQuestionChoices[2])}>{this.state.topQuestionChoices[2]}</button>
-                        <button className="waves-effect waves-light btn numberGameBtn" buttonId="btn-4" data-answerChoice={this.state.topQuestionChoices[3]} onClick={() => this.handleClick(this.state.topQuestionChoices[3])}>{this.state.topQuestionChoices[3]}</button>
-                    </form>
+                    <button className="waves-effect waves-light btn numberGameBtn" buttonId="btn-1" data-answerChoice={this.state.topQuestionChoices[0]} onClick={() => this.handleClick(this.state.topQuestionChoices[0])}>{this.state.topQuestionChoices[0]}</button>
+                    <button className="waves-effect waves-light btn numberGameBtn" buttonId="btn-2" data-answerChoice={this.state.topQuestionChoices[1]} onClick={() => this.handleClick(this.state.topQuestionChoices[1])}>{this.state.topQuestionChoices[1]}</button>
+                    <button className="waves-effect waves-light btn numberGameBtn" buttonId="btn-3" data-answerChoice={this.state.topQuestionChoices[2]} onClick={() => this.handleClick(this.state.topQuestionChoices[2])}>{this.state.topQuestionChoices[2]}</button>
+                    <button className="waves-effect waves-light btn numberGameBtn" buttonId="btn-4" data-answerChoice={this.state.topQuestionChoices[3]} onClick={() => this.handleClick(this.state.topQuestionChoices[3])}>{this.state.topQuestionChoices[3]}</button>
+                    
+                    <h3>Statistics</h3>
+                    <h4>Correct Answers: {this.state.correctCount}</h4>
+                    <h4>Incorrect Answers: {this.state.incorrectCount}</h4>
                 </div>
 
             </Fragment>
