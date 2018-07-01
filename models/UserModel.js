b4a99aa66bb88fd0f7dea4e3bc9fe95953bb79b9
Lance Lee <lancechoelee@gmail.com>
2018-07-01 10:16:24 -0700

    updated usermodel to reflect sign up sheet

--- models/UserModel.js
+++ models/UserModel.js
@@ -3,20 +3,28 @@ const Schema = mongoose.Schema
 const passportLocalMongoose = require('passport-local-mongoose');
 
 var UserSchema = new Schema ({
-    name : {
+    username : {
         type : String,
-        default: 'Quinchilla',
+        // default: 'Quinchilla', I dont think we actually need this, right?
         required : true
     },
     email : {
         type: String, 
-        default: 'abc@gmail.com',
+        // default: 'abc@gmail.com', I dont think we actually need this, right?
         required : true,
         unique : true
     },
+    password: {
+        type: String,
+        required: true
+    },
+    
+    childName: String,
+
     numberGame : {
         timesPlayed: Number,
-        scores: [Number],
+        correctCount: Number,
+        incorrectCount Number: 
     },
     memoryGame : {
         timesPlayed: Number,
