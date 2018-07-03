3950da0f91dea199e2f5c2645ea630babf3178a4
Laura Salcido <idlaurasalcido@gmail.com>
2018-07-03 10:22:43 -0700

    fixing heroku

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,6 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import Footer from './components/Footer';
-import AppRouter from './approuter'
 import './App.css';
-import Home from './components/Home'
-// import Footer from './components/Footer'
 
 class App extends Component {
     render() {
--- client/src/components/Games/Games.js
+++ client/src/components/Games/Games.js
@@ -13,16 +13,24 @@ class Games extends Component {
                 <div id='carouselDiv'>
                     <Carousel id='thisdiv'>
                         <Link id='cardgame' to='/games/cardgame' >
-                            <img src="http://25.io/smaller/img/smaller_256x256.png" alt="nature" />
+                            <img src="http://25.io/smaller/img/smaller_256x256.png" alt="card game" />
                             <span>Card Game</span>
                         </Link>
                         <Link id='colorgame' to='/games/colorgame'>
-                            <img src="http://25.io/smaller/img/smaller_256x256.png" alt="nature" />
+                            <img src="http://25.io/smaller/img/smaller_256x256.png" alt="color game" />
                             <span> Color Game</span>
                         </Link>
                         <Link id='numbergame' to='/games/numbergame'>
-                            <img src="http://25.io/smaller/img/smaller_256x256.png" alt="nature" />
+                            <img src="http://25.io/smaller/img/smaller_256x256.png" alt="number game" />
                             <span>Number Game</span>
+                        </Link>
+                        <Link id='shapegame' to='/games/shapegame'>
+                            <img src="http://25.io/smaller/img/smaller_256x256.png" alt="shapes game" />
+                            <span>Shape Game</span>
+                        </Link>
+                        <Link id='shapegame' to='/games/shapegame'>
+                            <img src="http://25.io/smaller/img/smaller_256x256.png" alt="shapes game" />
+                            <span>Shape Game</span>
                         </Link>
                     </Carousel>
                 </div>
